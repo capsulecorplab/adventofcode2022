b2c3f988df5bf92c7b3cf6142888099bfd4d0bc2
Sean Marquez <capsulecorplab@gmail.com>
2023-01-01 22:44:45 -0700

    add install script for pharo

---
+++ install_pharo.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+
+# Zeroconf script to automatically download everything needed to run Pharo, including VM and image for target platform. See https://pharo.org/download
+curl -L https://get.pharo.org/64 | bash
